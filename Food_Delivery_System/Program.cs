47a0acfbb0d42de12f84fe2e0e117264ece52ea9
dinesh05 <dineshkumarm1999@gmail.com>
2022-11-21 16:16:48 +0530

    hotel changes

--- Food_Delivery_System/Program.cs
+++ Food_Delivery_System/Program.cs
@@ -1,4 +1,6 @@
 using Food_Delivery.Models;
+using Food_Delivery.RepositoryInterface;
+using Food_Delivery.RepositoryService;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +11,14 @@ builder.Services.AddDbContext<FoodDeliveryDbContext>(OPtions =>
     OPtions.UseSqlServer(builder.Configuration.GetConnectionString("ConStr"));
 });
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<ICustomer, CustomerService>();
+builder.Services.AddScoped<IHotel, HotelService>();
+builder.Services.AddScoped<IFood, FoodService>();
+builder.Services.AddScoped<IOrders, OrdersService>();
+builder.Services.AddScoped<IOrderDetail, OrderDetailService>();
+builder.Services.AddScoped<IDeliveryPerson, DeliveryPersonService>();
+builder.Services.AddScoped<IOrderShipmentDetail, OrderShipmentDetailService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
