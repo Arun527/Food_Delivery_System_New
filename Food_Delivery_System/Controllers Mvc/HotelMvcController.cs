641f571a4f68d66ada7719a5ac7bc90120da8fd5
Arun527 <116302934+Arun527@users.noreply.github.com>
2022-12-08 10:07:36 +0530

    basket page

--- Food_Delivery_System/Controllers Mvc/HotelMvcController.cs
+++ Food_Delivery_System/Controllers Mvc/HotelMvcController.cs
@@ -8,6 +8,8 @@ using Microsoft.VisualBasic;
 
 namespace Food_Delivery.Controllers_Mvc
 {
+     [Route("api/[controller]")]
+    [ApiController]
     public class HotelMvcController : Controller
     {
         private readonly ILogger<HotelMvcController> _logger;
@@ -61,6 +63,16 @@ namespace Food_Delivery.Controllers_Mvc
             //}
             //TempData["AlertMessage"] = "The Hotel Already Exist.. !";
 
+        }
+
+        [HttpGet("{hotelName}")]
+        public IActionResult GetHotelByName(string hotelName)
+        {
+            
+            var hotel = _hotel.GetHotelDetailByName(hotelName);
+            
+
+            return View(hotel);
         }
         public IActionResult GetAll()
         {
---
+++ Food_Delivery_System/Controllers Mvc/OrderShipmentMvcController.cs
@@ -0,0 +1,39 @@
+﻿using Food_Delivery.RepositoryInterface;
+using Food_Delivery_System.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Food_Delivery_System.Controllers_Mvc
+{
+    public class OrderShipmentMvcController : Controller
+    {
+
+        private readonly ILogger<OrderShipmentMvcController> _logger;
+        ICustomer _customer;
+        IHotel _hotel;
+        IDeliveryPerson _deliveryPerson;
+        IOrderDetail _orderDetail;
+        IOrderShipmentDetail _orderShipmentDetail;
+        public OrderShipmentMvcController(ILogger<OrderShipmentMvcController> logger, ICustomer obj, IHotel hotel, IDeliveryPerson person, IOrderDetail orderDetail, IOrderShipmentDetail orderShipmentDetail)
+        {
+            _logger = logger;
+            _customer = obj;
+            _hotel = hotel;
+            _deliveryPerson = person;
+            _orderDetail = orderDetail;
+            _orderShipmentDetail = orderShipmentDetail;
+
+        }
+        public IActionResult AddShipment()
+        {
+            return View();
+        }
+
+        public IActionResult AddView()
+        {
+            OrderShipmentRequest shipment = new OrderShipmentRequest();
+            var delivery=_deliveryPerson.GetAllDeliveryPersons();
+
+            return View();
+        }
+    }
+}
