85c4887ac0caeb5cc35b56d94008392f9d59a63b
dinesh05 <dineshkumarm1999@gmail.com>
2022-12-09 10:49:04 +0530

    changes

--- Food_Delivery_Project/Customer.cs
+++ Food_Delivery_Project/Customer.cs
@@ -1,134 +1,132 @@
-//using Food_Delivery_System.Models;
-//using Microsoft.AspNetCore.Mvc;
-//using Moq;
-//using Xunit.Sdk;
-//using Food_Delivery.RepositoryInterface;
-//using Food_Delivery.Controllers;
-//using Food_Delivery.Models;
-//using System.Reflection;
-//using System.Net;
-//using System;
-//using Microsoft.AspNetCore.Http;
+using Food_Delivery_System.Models;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit.Sdk;
+using Food_Delivery.RepositoryInterface;
+using Food_Delivery.Controllers;
+using Food_Delivery.Models;
+using System.Reflection;
+using System.Net;
+using System;
+using Microsoft.AspNetCore.Http;
 
-//namespace Food_Delivery_Project
-//{
-//    public class CustomerTest
-//    {
-//        [Fact]
-//        public void GetAllCustomer_OkResult()
-//        {
-//            Customer obj = (new Customer
-//            {
-//                CustomerId = 1,
-//                Name = "Dineshkumar",
-//                ContactNumber = "9876543232",
-//                Gender = "Male",
-//                Email = "dhineshkumarm1999@gmail.com",
-//                Address = "80/7,middle street ,Madurai"
-//            });
-//            List<Customer> Lisobj = new List<Customer>();
-//            Lisobj.Add(obj);
-//            var mockservice = new Mock<ICustomer>();
-//            mockservice.Setup(x => x.GetAll()).Returns(Lisobj);
-//            var controller = new CustomerController(mockservice.Object);
+namespace Food_Delivery_Project
+{
+    public class CustomerTest
+    {
+        [Fact]
+        public void GetAllCustomer_OkResult()
+        {
+            Customer obj = (new Customer
+            {
+                CustomerId = 1,
+                Name = "Dineshkumar",
+                ContactNumber = "9876543232",
+                Gender = "Male",
+                Email = "dhineshkumarm1999@gmail.com",
+                Address = "80/7,middle street ,Madurai"
+            });
+            List<Customer> Lisobj = new List<Customer>();
+            Lisobj.Add(obj);
+            var mockservice = new Mock<ICustomer>();
+            mockservice.Setup(x => x.GetAll()).Returns(Lisobj);
+            var controller = new CustomerController(mockservice.Object);
 
-//            var okresult = controller.GetAll();
+            var okresult = controller.GetAll();
 
-//            Assert.IsType<OkObjectResult>(okresult);
-//        }
-
-
-//        //[Fact]
-//        //public void GetAllCustomer_NotFound()
-//        //{
-
-//        //    Customer obj = (new Customer
-//        //    {
-//        //        CustomerId = 1,
-//        //        Name = "Dineshkumar",
-//        //        ContactNumber = "9876543232",
-//        //        Gender = "Male",
-//        //        Email = "dhineshkumarm1999@gmail.com",
-//        //        Address = "80/7,middle street ,Madurai"
-//        //    });
-//        //    List<Customer> Lisobj = new List<Customer>();
-//        //    var mockservice = new Mock<ICustomer>();
-//        //    mockservice.Setup(x => x.GetAll()).Returns(Lisobj);
-//        //    var controller = new CustomerController(mockservice.Object);
-
-//        //    var exception = controller.GetAll();
-
-//        //    Assert.IsType<NotFoundResult>(exception);
-//        //}
+            Assert.IsType<OkObjectResult>(okresult);
+        }
 
 
+        //[Fact]
+        //public void GetAllCustomer_NotFound()
+        //{
 
-//        [Fact]
-//        public void GetCustomerDetailById_OkResult()
-//        {
+        //    Customer obj = (new Customer
+        //    {
+        //        CustomerId = 1,
+        //        Name = "Dineshkumar",
+        //        ContactNumber = "9876543232",
+        //        Gender = "Male",
+        //        Email = "dhineshkumarm1999@gmail.com",
+        //        Address = "80/7,middle street ,Madurai"
+        //    });
+        //    List<Customer> Lisobj = new List<Customer>();
+        //    var mockservice = new Mock<ICustomer>();
+        //    mockservice.Setup(x => x.GetAll()).Returns(Lisobj);
+        //    var controller = new CustomerController(mockservice.Object);
 
-//            Customer obj = (new Customer
-//            {
-//                CustomerId = 1,
-//                Name = "Dineshkumar",
-//                ContactNumber = "9876543232",
-//                Gender = "Male",
-//                Email = "dhineshkumarm1999@gmail.com",
-//                Address = "80/7,middle street ,Madurai"
-//            });
-//            List<Customer> Lisobj = new List<Customer>();
-//            var mockservice = new Mock<ICustomer>();
-//            mockservice.Setup(x => x.GetCustomerDetailById(It.IsAny<int>())).Returns(obj);
-//            var controller = new CustomerController(mockservice.Object);
+        //    var exception = controller.GetAll();
 
-//            var exception = controller.GetCustomerDetailById(2);
-
-//            Assert.IsType<OkObjectResult>(exception);
-//        }
-
-
-//        // [Fact]
-//        //public void GetCustomerDetailById_NotFoundResult()
-//        //{
-
-//        //    Customer obj = null;
-//        //    List<Customer> Lisobj = new List<Customer>();
-//        //    var mockservice = new Mock<ICustomer>();
-//        //    mockservice.Setup(x => x.GetCustomerDetailById(It.IsAny<int>())).Returns(obj);
-//        //    var controller = new CustomerController(mockservice.Object);
-
-//        //    var exception = controller.GetCustomerDetailById(2);
-
-
-//        //    Assert.Equals(exception);
-//        //}
+        //    Assert.IsType<NotFoundResult>(exception);
+        //}
 
 
 
+        [Fact]
+        public void GetCustomerDetailById_OkResult()
+        {
 
-//        [Fact]
-//        public void GetCustomerDetailByNumber_OkResult()
-//        {
+            Customer obj = (new Customer
+            {
+                CustomerId = 1,
+                Name = "Dineshkumar",
+                ContactNumber = "9876543232",
+                Gender = "Male",
+                Email = "dhineshkumarm1999@gmail.com",
+                Address = "80/7,middle street ,Madurai"
+            });
+            List<Customer> Lisobj = new List<Customer>();
+            var mockservice = new Mock<ICustomer>();
+            mockservice.Setup(x => x.GetCustomerDetailById(It.IsAny<int>())).Returns(obj);
+            var controller = new CustomerController(mockservice.Object);
 
-//            Customer obj = (new Customer
-//            {
-//                CustomerId = 1,
-//                Name = "Dineshkumar",
-//                ContactNumber = "9876543232",
-//                Gender = "Male",
-//                Email = "dhineshkumarm1999@gmail.com",
-//                Address = "80/7,middle street ,Madurai"
-//            });
-//            List<Customer> Lisobj = new List<Customer>();
-//            var mockservice = new Mock<ICustomer>();
-//            mockservice.Setup(x => x.GetCustomerDetailById(It.IsAny<int>())).Returns(obj);
-//            var controller = new CustomerController(mockservice.Object);
+            var exception = controller.GetCustomerDetailById(2);
 
-//            var exception = controller.GetCustomerDetailById(2);
+            Assert.IsType<OkObjectResult>(exception);
+        }
 
-//            Assert.IsType<OkObjectResult>(exception);
-//        }
 
+        // [Fact]
+        //public void GetCustomerDetailById_NotFoundResult()
+        //{
+
+        //    Customer obj = null;
+        //    List<Customer> Lisobj = new List<Customer>();
+        //    var mockservice = new Mock<ICustomer>();
+        //    mockservice.Setup(x => x.GetCustomerDetailById(It.IsAny<int>())).Returns(obj);
+        //    var controller = new CustomerController(mockservice.Object);
+
+        //    var exception = controller.GetCustomerDetailById(2);
+
+
+        //    Assert.Equals(exception);
+        //}
+
+
+
+        [Fact]
+        public void GetCustomerDetailByNumber_OkResult()
+        {
+
+            Customer obj = (new Customer
+            {
+                CustomerId = 1,
+                Name = "Dineshkumar",
+                ContactNumber = "9876543232",
+                Gender = "Male",
+                Email = "dhineshkumarm1999@gmail.com",
+                Address = "80/7,middle street ,Madurai"
+            });
+            List<Customer> Lisobj = new List<Customer>();
+            var mockservice = new Mock<ICustomer>();
+            mockservice.Setup(x => x.GetCustomerDetailById(It.IsAny<int>())).Returns(obj);
+            var controller = new CustomerController(mockservice.Object);
+
+            var exception = controller.GetCustomerDetailById(2);
+
+            Assert.IsType<OkObjectResult>(exception);
+        }
 
 
 
@@ -151,7 +149,8 @@
 
 
 
-//    }
+
+    }
 
 
-//}
+}
