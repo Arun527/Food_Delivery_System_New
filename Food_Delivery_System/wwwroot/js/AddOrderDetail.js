a394329b400ff7e79d2e79d44f5a0aa3cde3e8c2
Arun527 <116302934+Arun527@users.noreply.github.com>
2022-12-20 19:05:45 +0530

    allign

--- Food_Delivery_System/wwwroot/js/AddOrderDetail.js
+++ Food_Delivery_System/wwwroot/js/AddOrderDetail.js
@@ -4,50 +4,21 @@ var data = localStorage.getItem("cart");
 console.log("data", data);
 
 $.each(JSON.parse(data), function (key, value) {
-    $('#productList').append('<tr><td class="word">' + value.hotelname + '</td><td>' + value.foodname + '</td><td>' + ' <img class="foodimg" src="/css/Image/' + value.imageid + '" />' + '</td><td>' + value.price + '</td><td>' + value.quantity + '</td><td>' + value.quantity * value.price + '</td><td><button class="btn btn-danger delete ">Delete </button></td></tr>');
-
-
-
-
-
-
-
-
+    $('#productList').append('<tr><td class="word">' + value.hotelname + '</td><td>' + value.foodname + '</td><td>' +
+        ' <img class="foodimg" src="/css/Image/' + value.imageid + '" />' + '</td><td>' + value.price + '</td><td>' +
+        value.quantity + '</td><td>' + value.quantity * value.price + '</td><td><button class="btn btn-danger delete ">Delete </button></td></tr>');
 
     obj = { "FoodId": value.hotelId, "HotelId": value.foodId, "Quantity": value.quantity }
 
-
-
-
-
-
-
-
     foodDetails.push(obj);
 
-
-
-
-
-
-
-
-
-
 });
-
-
-
 
 $(document).on('click', '.delete', function () {
     let index = $(this).closest('tr').index() - 1;
     $(this).closest('tr').remove();
     console.log(index);
     foodDetails.splice(index, 1);
-    //console.log(productDetails);
-    //if (productDetails.length == 0) {
-    //    $('#productList').hide();
-    //}
 });
 
 
@@ -56,35 +27,21 @@ $(document).ready(function () {
     $('#btnsubmit').click(function (e) {
         e.preventDefault();
         var id = $('#customerid').val();
-
         if (id == null || id == 0) {
             $('#errormsg').html("Customer is required");
             return;
         }
-
         var Request = {
             "CustomerId": id,
             "Food": foodDetails
         };
-
-
-
-
-
         $.ajax({
-
-
-
             url: "/OrderDetailMvc/AddOrder",
             type: "POST",
             dataType: 'json',
             data: JSON.stringify(Request),
             contentType: 'application/json; charset=utf-8',
             success: function (response) {
-
-
-
-
                 if (response.success == true) {
                     localStorage.clear();
                     setTimeout(function () { window.location = '/OrderShipmentMvc/GetShipmentByUser?CustomerId=' + id; }, 1000);
@@ -93,17 +50,10 @@ $(document).ready(function () {
                     setTimeout(function () { window.location = '/OrderDetailMvc/Add'; }, 1000);
                     alert("Please choose delivery person");
                 }
-
-
-
-
             },
             error: function () {
                 alert("Pleae select a value");
             }
-
-
-
         });
 
     });
--- Food_Delivery_System/wwwroot/js/Addshipment.js
+++ Food_Delivery_System/wwwroot/js/Addshipment.js
@@ -1,81 +1,59 @@
-﻿
-    var obj;
-    var orderDetail=[];
-    var Request;
+﻿var obj;
+var orderDetail=[];
+var Request;
 
-    $(document).ready(function () {
+$(document).ready(function () {
 
-        $('#addid').click(function (e) {
+    $('#addid').click(function (e) {
 
-            e.preventDefault();
-            var id = $('#DeliveryPersonId').val();
-            var orderId = $('#OrderId').val();
-            if (DeliveryPersonId == null || DeliveryPersonId == 0) {
-                $('#errormsg').html("Delivery Person  is required");
-                return;
+        e.preventDefault();
+        var id = $('#DeliveryPersonId').val();
+        var orderId = $('#OrderId').val();
+        if (DeliveryPersonId == null || DeliveryPersonId == 0) {
+            $('#errormsg').html("Delivery Person  is required");
+            return;
+        }
+        obj = { "OrderDetailId": orderId }
+
+        $('#idList').append('<tr><td>' + orderId + '</td><td><button class="btn btn-danger delete">Delete </button></td></tr>');
+
+        orderDetail.push(obj);
+
+        $(document).on('click', '.delete', function () {
+            let index = $(this).closest('tr').index() - 1;
+            $(this).closest('tr').remove();
+            console.log(index);
+            productDetails.splice(index, 1);
+            console.log(productDetails);
+            if (orderDetail.length == 0) {
+                $('#idList').hide();
             }
-
-
-            obj = { "OrderDetailId": orderId }
-
-            $('#idList').append('<tr><td>' + orderId + '</td><td><button class="btn btn-danger delete">Delete </button></td></tr>');
-
-            orderDetail.push(obj);
-
-            $(document).on('click', '.delete', function () {
-                let index = $(this).closest('tr').index() - 1;
-                $(this).closest('tr').remove();
-                console.log(index);
-                productDetails.splice(index, 1);
-                console.log(productDetails);
-                if (orderDetail.length == 0) {
-                    $('#idList').hide();
-                }
-            });
-
-            Request = {
-                "DeliveryPersonId": id,
-                "ShipmentRequest": orderDetail
-
-
-
-            };
-
-
         });
 
+        Request = {
+            "DeliveryPersonId": id,
+            "ShipmentRequest": orderDetail
+        };
+    });
+$('#btnsubmit').click(function (e) {
+    $.ajax({
+        url: "/OrderShipmentMvc/Add",
+        type: "POST",
+        dataType: 'json',
+        data: JSON.stringify(Request),
+        contentType: 'application/json; charset=utf-8',
+        success: function (response) {
 
-    $('#btnsubmit').click(function (e) {
+            alert("Order is Succesfully Shipped");
+            window.location.href = "/OrderShipmentMvc/GetAllShipment";
+        },
+        error: function () {
+            alert("error");
+        }
+    });
 
-
-
-
-        $.ajax({
-
-
-            url: "/OrderShipmentMvc/Add",
-            type: "POST",
-            dataType: 'json',
-            data: JSON.stringify(Request),
-            contentType: 'application/json; charset=utf-8',
-            success: function (response) {
-
-                alert("Order is Succesfully Shipped");
-                window.location.href = "/OrderShipmentMvc/GetAllShipment";
-
-
-
-            },
-            error: function () {
-                alert("error");
-            }
-
-
-
-        });
-
-                });
-            });
+    });
+});
 
 
 
