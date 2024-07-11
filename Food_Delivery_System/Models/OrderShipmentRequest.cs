6fce6756be9a6a6a099c0016610b2a5204d91899
Arunachalam S <arunachalam.subramani@amtote.com>
2024-07-11 13:06:24 +0530

    feat: add nullable

--- Food_Delivery_System/Models/OrderShipmentRequest.cs
+++ Food_Delivery_System/Models/OrderShipmentRequest.cs
@@ -7,7 +7,7 @@
 
         [System.ComponentModel.DataAnnotations.Range(1, 2147483647)]
         public int DeliveryPersonId { get; set; }
-        public List<OrderShipmentList> ShipmentRequest { get; set; }    
+        public List<OrderShipmentList>? ShipmentRequest { get; set; }    
     }
 
     public class OrderShipmentList
